f16e5a5f9ad01efaff416e7167d06421b329335a
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2025-10-20 10:33:17 +0100

    <feat>(row creation) created the row class to contain a vector of elements. Allows for you to pass a r value into constructor and will sort values by col. allows for you to print values in row

--- include/spira/row.hpp
+++ include/spira/row.hpp
@@ -1,7 +1,28 @@
 #pragma once
 
+#include <algorithm>
+#include <iostream>
+#include <vector>
 #include "element.hpp"
 
 namespace spira::row {
-
+    template<concepts::Valueable V, concepts::Indexable I>
+    class row {
+        private:
+            std::vector<element::element<V, I>> _row;
+        public:
+            explicit row(std::vector<element::element<V, I>>&& row) {
+                _row.reserve(row.size());
+                _row = std::move(row);
+                std::sort(_row.begin(), _row.end(), [](element::element<V, I> a, element::element<V, I> b) {
+                    return a.col < b.col;
+                });
+            }
+            void printRow() const {
+                for (const auto& element : _row) {
+                    std::cout <<"(" << element.value << ", " << element.col << ") ";
+                }
+            }
+            ~row() = default;
+};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,6 @@
+#include "spira/row.hpp"
 #include "spira/spira.hpp"
+int main() {
+    const spira::row::row<int, size_t> r({{3,4},{5,6}, {1,2}});
+    r.printRow();
+}
