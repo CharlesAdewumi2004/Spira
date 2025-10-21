f9ffdd72758d4ef4bfedaa277018aac391081fa9
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2025-10-21 18:05:25 +0100

    <refactor>(row and matrix) changed how row and matrix interact and added functionality to add element to matrix and added error checking

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "spira/matrix.hpp"
 #include "spira/row.hpp"
 #include "spira/spira.hpp"
-
+/*
 int main() {
-    spira::matrix<size_t, size_t, 2, 2> matrix{};
-    matrix.tryFillRow(0, {{1,2}, {3,4}, {7,16}});
+    spira::matrix<size_t, size_t, 10, 10> matrix{};
+    matrix.tryFillRow(0, {{1,2}, {3,2}, {7,16}});
     matrix.printRow(0);
-}
+}*/
