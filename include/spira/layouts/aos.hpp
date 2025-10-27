d2bbe6de1b410b7d4cec7f615071a8865a598491
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2025-10-27 09:53:31 +0000

    <feat> created layer implementations for AoS and Soa

--- include/spira/layouts/aos.hpp
+++ include/spira/layouts/aos.hpp
@@ -1,5 +1,61 @@
 #pragma once
+#include <vector>
+#include <algorithm>
+
+#include "spira/concepts.hpp"
 
 namespace spira::layout {
-    struct aos{};
+    template<concepts::Indexable I, concepts::Valueable V>
+    struct elementPair {
+        I column;
+        V value;
+    };
+
+    template<concepts::Indexable I, concepts::Valueable V>
+    class aos {
+        private:
+            std::vector<elementPair<I, V>> elements;
+        public:
+            [[nodiscard]] bool empty() const noexcept {
+                return elements.empty();
+            }
+            void reserve(size_t size) {
+                elements.reserve(size);
+            }
+            [[nodiscard]] size_t capacity() const noexcept {
+                return elements.capacity();
+            }
+            [[nodiscard]] size_t size() const noexcept {
+                return elements.size();
+            }
+            void clear() {
+                elements.clear();
+            }
+            I key_at(size_t index) const noexcept {
+                return elements[index].column;
+            }
+            [[nodiscard]] V& value_at(size_t pos) noexcept {
+                    return elements[pos].value;
+                }
+
+            [[nodiscard]] const V& value_at(size_t pos) const noexcept {
+                    return elements[pos].value;
+                }
+            void set_at(size_t index, I col, V val) {
+                elements[index] = elementPair<I,V>{col, val};
+            }
+            void insert_at(size_t index, I col, V val) {
+                elements.insert_at(index, elementPair<I,V>{col, val});
+            }
+            void erase_at(size_t index) {
+                elements.erase_at(elements.begin() + index);
+            }
+            [[nodiscard]] size_t lower_bound(I col) const noexcept {
+                auto it = std::lower_bound(
+                    elements.begin(), elements.end(), col,
+                    [](auto const& e, I key){ return e.column < key; }
+                );
+                return static_cast<size_t>(std::distance(elements.begin(), it));
+            }
+    };
 }
--- include/spira/layouts/soa.hpp
+++ include/spira/layouts/soa.hpp
@@ -1,5 +1,64 @@
 #pragma once
 
+#include <vector>
+#include <algorithm>
+
+#include "spira/concepts.hpp"
+
 namespace spira::layout {
-    struct soa{};
+    template<concepts::Indexable I, concepts::Valueable V>
+    class soa {
+        private:
+            std::vector<I> columns;
+            std::vector<V> values;
+
+        public:
+            [[nodiscard]] bool empty() const noexcept {
+                return  columns.empty();
+            }
+            [[nodiscard]] size_t size() const noexcept {
+                return columns.size();
+            }
+            void clear() noexcept {
+                columns.clear();
+                values.clear();
+            }
+            void reserve(size_t size) {
+                columns.reserve(size);
+                values.reserve(size);
+            }
+            [[nodiscard]] size_t capacity() const noexcept {
+                return columns.capacity();
+            }
+            [[nodiscard]] I key_at(size_t index) const noexcept {
+                return columns[index];
+            }
+            [[nodiscard]] V& value_at(size_t pos) noexcept {
+                    return values[pos];
+                }
+
+            [[nodiscard]] const V& value_at(size_t pos) const noexcept {
+                    return values[pos];
+                }
+            void set_at(size_t index, I col,  V val)  {
+                columns[index] = col;
+                values[index] = val;
+            }
+            void insert_at(size_t index, I col, V val)  {
+                columns.insert(columns.begin() + index, col);
+                values.insert(values.begin() + index, val);
+            }
+            void erase_at(size_t index)  {
+                columns.erase(columns.begin() + index);
+                values.erase(values.begin() + index);
+            }
+            [[nodiscard]] size_t lower_bound(I col) const noexcept {
+                auto it = std::lower_bound(columns.begin(), columns.end(), col);
+                if (it == columns.end()) {
+                    return columns.size();
+                }else {
+                    return static_cast<size_t>(std::distance(columns.begin(), it));
+                }
+            }
+    };
 }
