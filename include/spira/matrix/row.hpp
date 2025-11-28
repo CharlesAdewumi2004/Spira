1338605f2460c62e7523a84251d72f73b7bed250
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2025-11-28 19:23:17 +0000

    fixed version  where row can be seen

--- include/spira/matrix/row.hpp
+++ include/spira/matrix/row.hpp
@@ -8,8 +8,7 @@
 #include "spira/traits.hpp"
 #include "layouts/layout_of.hpp"
 
-namespace spira::row
-{
+namespace spira{
 
     template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
     class row
@@ -37,193 +36,193 @@ namespace spira::row
         [[nodiscard]] const V *get(I col) const;
 
         template <class Func>
-        void row<LayoutTag, I, V>::for_each_nnz(const Func &c) const
+        void for_each_nnz(const Func &&f) const;
+
+    private:
+        layout_policy storage_;
+        size_t const column_limit_;
+    };
+
+    // -----------------------------------------------------------------------------
+    // Implementation
+    // -----------------------------------------------------------------------------
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    row<LayoutTag, I, V>::row()
+        : column_limit_(0)
+    {
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    row<LayoutTag, I, V>::row(std::size_t reserve_hint, size_t const column_limit)
+        : column_limit_(column_limit)
+    {
+        storage_.reserve(reserve_hint);
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    bool row<LayoutTag, I, V>::empty() const noexcept
+    {
+        return storage_.empty();
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    std::size_t row<LayoutTag, I, V>::size() const noexcept
+    {
+        return storage_.size();
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    std::size_t row<LayoutTag, I, V>::capacity() const noexcept
+    {
+        return storage_.capacity();
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    void row<LayoutTag, I, V>::reserve(std::size_t n)
+    {
+        storage_.reserve(n);
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    void row<LayoutTag, I, V>::clear() noexcept
+    {
+        storage_.clear();
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    void row<LayoutTag, I, V>::add(I col, const V &val)
+    {
+        if (col >= column_limit_)
+            return;
+
+        const bool is_zero = traits::ValueTraits<V>::is_zero(val);
+        std::size_t pos = storage_.lower_bound(col);
+
+        if (pos < storage_.size() && storage_.key_at(pos) == col)
         {
-
-        private:
-            layout_policy storage_;
-            size_t const column_limit_;
-        };
-
-        // -----------------------------------------------------------------------------
-        // Implementation
-        // -----------------------------------------------------------------------------
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        row<LayoutTag, I, V>::row()
-            : column_limit_(0)
-        {
-        }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        row<LayoutTag, I, V>::row(std::size_t reserve_hint, size_t const column_limit)
-            : column_limit_(column_limit)
-        {
-            storage_.reserve(reserve_hint);
-        }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        bool row<LayoutTag, I, V>::empty() const noexcept
-        {
-            return storage_.empty();
-        }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        std::size_t row<LayoutTag, I, V>::size() const noexcept
-        {
-            return storage_.size();
-        }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        std::size_t row<LayoutTag, I, V>::capacity() const noexcept
-        {
-            return storage_.capacity();
-        }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        void row<LayoutTag, I, V>::reserve(std::size_t n)
-        {
-            storage_.reserve(n);
-        }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        void row<LayoutTag, I, V>::clear() noexcept
-        {
-            storage_.clear();
-        }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        void row<LayoutTag, I, V>::add(I col, const V &val)
-        {
-            if (col >= column_limit_)
-                return;
-
-            const bool is_zero = traits::ValueTraits<V>::is_zero(val);
-            std::size_t pos = storage_.lower_bound(col);
-
-            if (pos < storage_.size() && storage_.key_at(pos) == col)
-            {
-                if (is_zero)
-                {
-                    storage_.erase_at(pos);
-                }
-                else
-                {
-                    storage_.value_at(pos) = val;
-                }
-            }
-            else
-            {
-                if (!is_zero)
-                {
-                    storage_.insert_at(pos, col, val);
-                }
-            }
-        }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        void row<LayoutTag, I, V>::remove(I col)
-        {
-            if (col >= column_limit_)
-                return;
-
-            std::size_t pos = storage_.lower_bound(col);
-            if (pos < storage_.size() && storage_.key_at(pos) == col)
+            if (is_zero)
             {
                 storage_.erase_at(pos);
             }
+            else
+            {
+                storage_.value_at(pos) = val;
+            }
         }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        template <class PairRange>
-        void row<LayoutTag, I, V>::set_row(const PairRange &pairs)
+        else
         {
-            std::vector<std::pair<I, V>> tmp;
-            tmp.reserve(std::distance(std::begin(pairs), std::end(pairs)));
-
-            for (auto &&p : pairs)
+            if (!is_zero)
             {
-                const I c = static_cast<I>(p.first);
-                const V &v = p.second;
-
-                if (c < column_limit_ && !traits::ValueTraits<V>::is_zero(v))
-                {
-                    tmp.emplace_back(c, v);
-                }
+                storage_.insert_at(pos, col, val);
             }
+        }
+    }
 
-            std::sort(
-                tmp.begin(), tmp.end(),
-                [](auto const &a, auto const &b)
-                {
-                    return a.first < b.first;
-                });
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    void row<LayoutTag, I, V>::remove(I col)
+    {
+        if (col >= column_limit_)
+            return;
 
-            std::vector<std::pair<I, V>> cleaned;
-            cleaned.reserve(tmp.size());
+        std::size_t pos = storage_.lower_bound(col);
+        if (pos < storage_.size() && storage_.key_at(pos) == col)
+        {
+            storage_.erase_at(pos);
+        }
+    }
 
-            for (std::size_t i = 0; i < tmp.size();)
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    template <class PairRange>
+    void row<LayoutTag, I, V>::set_row(const PairRange &pairs)
+    {
+        std::vector<std::pair<I, V>> tmp;
+        tmp.reserve(std::distance(std::begin(pairs), std::end(pairs)));
+
+        for (auto &&p : pairs)
+        {
+            const I c = static_cast<I>(p.first);
+            const V &v = p.second;
+
+            if (c < column_limit_ && !traits::ValueTraits<V>::is_zero(v))
             {
-                I c = tmp[i].first;
-                V v = tmp[i].second;
-
-                std::size_t j = i + 1;
-                while (j < tmp.size() && tmp[j].first == c)
-                {
-                    v = tmp[j].second;
-                    ++j;
-                }
-
-                if (!traits::ValueTraits<V>::is_zero(v))
-                {
-                    cleaned.emplace_back(c, v);
-                }
-
-                i = j;
-            }
-
-            storage_.clear();
-            storage_.reserve(cleaned.size());
-
-            for (auto &[c, v] : cleaned)
-            {
-                storage_.insert_at(storage_.size(), c, v);
+                tmp.emplace_back(c, v);
             }
         }
 
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        bool row<LayoutTag, I, V>::contains(I col) const
-        {
-            if (col >= column_limit_)
-                return false;
-
-            std::size_t pos = storage_.lower_bound(col);
-            return (pos < storage_.size() && storage_.key_at(pos) == col);
-        }
-
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        const V *row<LayoutTag, I, V>::get(I col) const
-        {
-            if (col >= column_limit_)
-                return nullptr;
-
-            std::size_t pos = storage_.lower_bound(col);
-
-            if (pos < storage_.size() && storage_.key_at(pos) == col)
+        std::sort(
+            tmp.begin(), tmp.end(),
+            [](auto const &a, auto const &b)
             {
-                return &storage_.value_at(pos);
+                return a.first < b.first;
+            });
+
+        std::vector<std::pair<I, V>> cleaned;
+        cleaned.reserve(tmp.size());
+
+        for (std::size_t i = 0; i < tmp.size();)
+        {
+            I c = tmp[i].first;
+            V v = tmp[i].second;
+
+            std::size_t j = i + 1;
+            while (j < tmp.size() && tmp[j].first == c)
+            {
+                v = tmp[j].second;
+                ++j;
             }
 
+            if (!traits::ValueTraits<V>::is_zero(v))
+            {
+                cleaned.emplace_back(c, v);
+            }
+
+            i = j;
+        }
+
+        storage_.clear();
+        storage_.reserve(cleaned.size());
+
+        for (auto &[c, v] : cleaned)
+        {
+            storage_.insert_at(storage_.size(), c, v);
+        }
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    bool row<LayoutTag, I, V>::contains(I col) const
+    {
+        if (col >= column_limit_)
+            return false;
+
+        std::size_t pos = storage_.lower_bound(col);
+        return (pos < storage_.size() && storage_.key_at(pos) == col);
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    const V *row<LayoutTag, I, V>::get(I col) const
+    {
+        if (col >= column_limit_)
             return nullptr;
+
+        std::size_t pos = storage_.lower_bound(col);
+
+        if (pos < storage_.size() && storage_.key_at(pos) == col)
+        {
+            return &storage_.value_at(pos);
         }
 
-        template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
-        template <class Func>
-        void row<LayoutTag, I, V>::for_each_nnz(Func &&f) const
+        return nullptr;
+    }
+
+    template <class LayoutTag, concepts::Indexable I, concepts::Valueable V>
+    template <class Func>
+    void row<LayoutTag, I, V>::for_each_nnz(const Func &&f) const
+    {
+        for (auto it = storage_.cbegin(); it != storage_.cend(); it++)
         {
-            for (auto &elem : storage_)
-            {
-                std::forward<Func>(f)(elem);
-            }
+            auto [col, value] = *it;
+            f(col, value);
         }
-    };
+    }
+};
