8506e772dd4b69e194f5a51a21f3d0cf31e8e254
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2026-03-03 18:01:43 +0000

    changed simd avx to use sclaer gathering not ISA gathering

--- src/kernels/dot_impls/x86/dot_avx.cpp
+++ src/kernels/dot_impls/x86/dot_avx.cpp
@@ -7,10 +7,13 @@ double sparse_dot_double_avx(const double *vals, const uint32_t *cols, const dou
     size_t i = 0;
     spira::kernel::simd::reg256_double acc_reg = spira::kernel::simd::zero_double_256();
     
-    for (; i + 4 <= n; i += 4) { // <= not
+    for (; i + 4 <= n; i += 4) {
         spira::kernel::simd::reg256_double v = spira::kernel::simd::load_double_256(vals + i);
-        spira::kernel::simd::reg128_int idx = spira::kernel::simd::load_int_128(cols + i);
-        spira::kernel::simd::reg256_double xv = spira::kernel::simd::gather_double_256(x, idx);
+
+        float vx0 = x[cols[i]], vx1 = x[cols[i+1]], vx2 = x[cols[i+2]], vx3 = x[cols[i+3]];
+
+        spira::kernel::simd::reg256_double xv = _mm256_setr_pd(vx0, vx1, vx2, vx3);
+
         acc_reg = spira::kernel::simd::fma_double_256(v, xv, acc_reg);
     }
 
@@ -27,10 +30,13 @@ float sparse_dot_float_avx(const float *vals, const uint32_t *cols, const float 
     size_t i = 0;
     spira::kernel::simd::reg256_float acc_reg = spira::kernel::simd::zero_float_256();
 
-    for (; i + 8 <= n; i += 8) { // i + 8 <= n, not i < n
+    for (; i + 8 <= n; i += 8) {
         spira::kernel::simd::reg256_float v = spira::kernel::simd::load_float_256(vals + i);
-        spira::kernel::simd::reg256_int idx = spira::kernel::simd::load_int_256((const int *)(cols + i));
-        spira::kernel::simd::reg256_float xv = spira::kernel::simd::gather_float_256(x, idx);
+        float vx0 = x[cols[i]], vx1 = x[cols[i+1]], vx2 = x[cols[i+2]], vx3 = x[cols[i+3]], 
+        vx4 = x[cols[i+4]], vx5 = x[cols[i+5]], vx6 = x[cols[i+6]], vx7 = x[cols[i+7]];
+
+        spira::kernel::simd::reg256_float xv = _mm256_setr_ps(vx0, vx1, vx2, vx3, vx4, vx5, vx6, vx7);
+
         acc_reg = spira::kernel::simd::fma_float_256(v, xv, acc_reg);
     }
 
