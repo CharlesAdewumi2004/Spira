09ecd03400b254ffb9ab4794a2968655ec4c5df4
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2026-04-10 10:52:10 +0100

    fix: correct scalers->scalars include and disable benchmark Werror on Windows

--- include/spira/spira.hpp
+++ include/spira/spira.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <spira/algorithms/matrix_addition.hpp>
 #include <spira/algorithms/merge.hpp>
-#include <spira/algorithms/scalers.hpp>
+#include <spira/algorithms/scalars.hpp>
 #include <spira/algorithms/spgemm.hpp>
 #include <spira/algorithms/spmv.hpp>
 #include <spira/algorithms/transpose.hpp>
