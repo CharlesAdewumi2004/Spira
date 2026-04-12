7951cf647b33883ab678bdcf79ade31944a797f6
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2026-04-12 22:01:46 +0100

    using gather instead of scalers for input vecotr memeory loads

--- src/kernels/dot_impls/x86/dot_avx512.cpp
+++ src/kernels/dot_impls/x86/dot_avx512.cpp
@@ -26,21 +26,15 @@ double sparse_dot_double_avx512(const double* vals, const uint32_t* cols, const 
             __builtin_prefetch(&x[cols[i + 7 + d]], 0, 3);
 
             __m512d v = _mm512_loadu_pd(vals + i);
-
-            __m512d xv = _mm512_setr_pd(
-                x[cols[i]], x[cols[i+1]], x[cols[i+2]], x[cols[i+3]],
-                x[cols[i+4]], x[cols[i+5]], x[cols[i+6]], x[cols[i+7]]);
-
+            __m256i idx = _mm256_loadu_si256((const __m256i *)(cols + i));
+            __m512d xv = _mm512_i32gather_pd(idx, x, 8);
             acc_reg = _mm512_fmadd_pd(v, xv, acc_reg);
         }
 
         for (; i + 8 <= n; i += 8) {
             __m512d v = _mm512_loadu_pd(vals + i);
-
-            __m512d xv = _mm512_setr_pd(
-                x[cols[i]], x[cols[i+1]], x[cols[i+2]], x[cols[i+3]],
-                x[cols[i+4]], x[cols[i+5]], x[cols[i+6]], x[cols[i+7]]);
-
+            __m256i idx = _mm256_loadu_si256((const __m256i *)(cols + i));
+            __m512d xv = _mm512_i32gather_pd(idx, x, 8);
             acc_reg = _mm512_fmadd_pd(v, xv, acc_reg);
         }
 
@@ -54,11 +48,8 @@ double sparse_dot_double_avx512(const double* vals, const uint32_t* cols, const 
     {
         for (; i + 8 <= n; i += 8) {
             __m512d v = _mm512_loadu_pd(vals + i);
-
-            __m512d xv = _mm512_setr_pd(
-                x[cols[i]], x[cols[i+1]], x[cols[i+2]], x[cols[i+3]],
-                x[cols[i+4]], x[cols[i+5]], x[cols[i+6]], x[cols[i+7]]);
-
+            __m256i idx = _mm256_loadu_si256((const __m256i *)(cols + i));
+            __m512d xv = _mm512_i32gather_pd(idx, x, 8);
             acc_reg = _mm512_fmadd_pd(v, xv, acc_reg);
         }
 
@@ -101,25 +92,15 @@ float sparse_dot_float_avx512(const float* vals, const uint32_t* cols, const flo
             __builtin_prefetch(&x[cols[i + 15 + d]], 0, 3);
 
             __m512 v = _mm512_loadu_ps(vals + i);
-
-            __m512 xv = _mm512_setr_ps(
-                x[cols[i]],    x[cols[i+1]],  x[cols[i+2]],  x[cols[i+3]],
-                x[cols[i+4]],  x[cols[i+5]],  x[cols[i+6]],  x[cols[i+7]],
-                x[cols[i+8]],  x[cols[i+9]],  x[cols[i+10]], x[cols[i+11]],
-                x[cols[i+12]], x[cols[i+13]], x[cols[i+14]], x[cols[i+15]]);
-
+            __m512i idx = _mm512_loadu_si512((const __m512i *)(cols + i));
+            __m512 xv = _mm512_i32gather_ps(idx, x, 4);
             acc_reg = _mm512_fmadd_ps(v, xv, acc_reg);
         }
 
         for (; i + 16 <= n; i += 16) {
             __m512 v = _mm512_loadu_ps(vals + i);
-
-            __m512 xv = _mm512_setr_ps(
-                x[cols[i]],    x[cols[i+1]],  x[cols[i+2]],  x[cols[i+3]],
-                x[cols[i+4]],  x[cols[i+5]],  x[cols[i+6]],  x[cols[i+7]],
-                x[cols[i+8]],  x[cols[i+9]],  x[cols[i+10]], x[cols[i+11]],
-                x[cols[i+12]], x[cols[i+13]], x[cols[i+14]], x[cols[i+15]]);
-
+            __m512i idx = _mm512_loadu_si512((const __m512i *)(cols + i));
+            __m512 xv = _mm512_i32gather_ps(idx, x, 4);
             acc_reg = _mm512_fmadd_ps(v, xv, acc_reg);
         }
 
@@ -133,13 +114,8 @@ float sparse_dot_float_avx512(const float* vals, const uint32_t* cols, const flo
     {
         for (; i + 16 <= n; i += 16) {
             __m512 v = _mm512_loadu_ps(vals + i);
-
-            __m512 xv = _mm512_setr_ps(
-                x[cols[i]],    x[cols[i+1]],  x[cols[i+2]],  x[cols[i+3]],
-                x[cols[i+4]],  x[cols[i+5]],  x[cols[i+6]],  x[cols[i+7]],
-                x[cols[i+8]],  x[cols[i+9]],  x[cols[i+10]], x[cols[i+11]],
-                x[cols[i+12]], x[cols[i+13]], x[cols[i+14]], x[cols[i+15]]);
-
+            __m512i idx = _mm512_loadu_si512((const __m512i *)(cols + i));
+            __m512 xv = _mm512_i32gather_ps(idx, x, 4);
             acc_reg = _mm512_fmadd_ps(v, xv, acc_reg);
         }
 
